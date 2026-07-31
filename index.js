823e90017e56ec8daa7b6732a22c67739e8aa833
richard rodriguez <alexander1x11@gmail.com>
2026-07-31 21:12:23 +0200

    Update app version to 1.3 and version code to 4

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const PORT = process.env.PORT || 3000;
 // Ruta única para la versión de la app
 app.get('/api/version-app', (req, res) => {
     res.json({
-        versionName: "1.2",
-        versionCode: 3,
+        versionName: "1.3",
+        versionCode: 4,
         updateUrl: "https://soundypc.com/dolar-hoy-la-herramienta-definitiva-para-el-control-cambiario-y-financiero-en-venezuela/"
     });
 });
